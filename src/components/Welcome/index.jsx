cb0465a0ba7cd8ef1d4dd63f3f44941eb4d9fac8
sumanyadav9518@gmail.com <sumanyadav9518@gmail.com>
2019-12-14 10:05:17 +0100

    Pagination & link to each content

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -27,6 +27,7 @@ class Welcome extends React.Component {
         nextUrl={this.state.articles.next_page_url}
         prevUrl={this.state.articles.prev_page_url}
         handlePagination={this.handlePagination}
+
       />
     )
   }
